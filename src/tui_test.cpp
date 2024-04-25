f914bc796a02e59aa8d589ad1822e4f8fd7eda02
Gabbyss <160082866+Gabbyss@users.noreply.github.com>
2024-04-25 00:57:59 +0000

    Version 3

--- src/tui_test.cpp
+++ src/tui_test.cpp
@@ -5,6 +5,8 @@
 #include <thread>
 #include <ftxui/screen/color.hpp>
 #include <experimental/random>
+#include <list>
+#include <fstream>
 
 using namespace std;
 using namespace ftxui;
@@ -12,8 +14,25 @@ using namespace ftxui;
 int main(int argc, char const *argv[])
 
 {
+    fstream imagen;
+    list<string> textos;
+
+    imagen.open("./assets/imagen.txt");
+    string linea;
+
+    while (getline(imagen, linea))
+    {
+        textos.push_back(linea);
+    }
+
+    getline(imagen, linea);
+    imagen.close();
+
     int fotograma = 0;
+
     string reset;
+    int posX = 0;
+    int posY = 0;
 
     while (true)
     {
@@ -26,14 +45,28 @@ int main(int argc, char const *argv[])
         Element personaje = spinner(21, fotograma);
         Decorator colorFondo = bgcolor(Color::RGB(R, G, B));
         Decorator colorTexto = color(Color::RGB(B, G, R));
-        Element dibujo = border({hbox(personaje)}) | colorFondo | colorTexto;
+        Element dibujo = border({hbox()}) | colorFondo | colorTexto;
 
-        Dimensions Alto = Dimension::Fixed(7);
-        Dimensions Ancho = Dimension::Fixed(15);
+        Dimensions Alto = Dimension::Full();
+        Dimensions Ancho = Dimension::Full();
 
         Screen pantalla = Screen::Create(Ancho, Alto);
 
         Render(pantalla, dibujo);
+
+        int l = 0;
+        for (auto &&texto : textos)
+        {
+            int i = 0;
+            for (auto &&letra : texto)
+            {
+                pantalla.PixelAt(posX + i, posY + l).character = letra;
+                i++;
+            }
+            l++;
+        }
+
+        posX++;
 
         pantalla.Print();
         reset = pantalla.ResetPosition();
